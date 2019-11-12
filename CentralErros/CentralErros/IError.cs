b89f70b7e538dba00e49bd4dc232b76de5a17207
goisjulia <goisjulia@gmail.com>
2019-11-12 00:47:09 -0300

    WIP Troca de branch

--- CentralErros/CentralErros/IError.cs
+++ CentralErros/CentralErros/IError.cs
@@ -6,7 +6,7 @@ namespace CentralErros
 {
     interface IError
     {
-        // cadsatro de novo erro que retorna se houve sucesso ou não
+        // cadastro de novo erro que retorna se houve sucesso ou não
         bool CadastraErro(int id, int idAmbiente, int idLevel, int idSituacai, string titulo);
         
         // pesquisa po erro e o retorna se ele existe
