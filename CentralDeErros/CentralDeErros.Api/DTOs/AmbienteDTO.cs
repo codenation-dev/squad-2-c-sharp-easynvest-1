8068a70befeab2a93e7a1635b65ff6a25d1691cc
goisjulia <goisjulia@gmail.com>
2019-11-21 02:03:56 -0300

    Adição das últimas DTOs

---
+++ CentralDeErros/CentralDeErros.Api/DTOs/AmbienteDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentralDeErros.Api.DTOs
+{
+    public class AmbienteDTO
+    {
+        public int Ambiente_Id { get; set; }
+
+        [Required]
+        public string Nome_Ambiente { get; set; }
+    }
+}
---
+++ CentralDeErros/CentralDeErros.Api/DTOs/LevelDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentralDeErros.Api.DTOs
+{
+    public class LevelDTO
+    {
+        public int Level_Id { get; set; }
+
+        [Required]
+        public string Nome_Level { get; set; }
+
+    }
+}
